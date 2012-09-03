224582dfe22029ad99c8a9daffcc4d922b094711
basti <sk@dotob.de>
2012-09-03 16:09:37 +0200

    fix assemblyinfo, set version to 0.3

--- MONI/Properties/AssemblyInfo.cs
+++ MONI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonlistClone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("moni")]
+[assembly: AssemblyDescription("the better way to enter monlist data")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MonlistClone")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("basti")]
+[assembly: AssemblyProduct("moni")]
+[assembly: AssemblyCopyright("Copyright © basti 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
