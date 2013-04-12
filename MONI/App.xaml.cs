57bf87d0b05e4f09a88273dedbc401d726d0b251
Jan Karger <punkerat76@jkarger.de>
2013-04-12 23:27:40 +0200

    add NLog.2.0.1.2
    and move AssemblyResolve to Program.cs

--- MONI/App.xaml.cs
+++ MONI/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Windows;
+﻿using System.Windows;
 
 namespace MONI
 {
@@ -10,18 +8,6 @@ namespace MONI
   public partial class App : Application
   {
     protected override void OnStartup(StartupEventArgs e) {
-      AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
-        {
-          String resourceName = "MONI.DllsAsResource." + new AssemblyName(args.Name).Name + ".dll";
-          using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)) {
-            if (stream != null) {
-              Byte[] assemblyData = new Byte[stream.Length];
-              stream.Read(assemblyData, 0, assemblyData.Length);
-              return Assembly.Load(assemblyData);
-            }
-          }
-          return null;
-        };
       base.OnStartup(e);
     }
   }
