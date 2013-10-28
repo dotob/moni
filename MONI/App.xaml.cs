49256e8b7ebc14322a362a11cd733fd3aaa17769
punker76 <punkerat76@gmail.com>
2013-10-28 15:55:11 +0100

    use MahApps.Metro.0.11.2.1-ALPHA

--- MONI/App.xaml.cs
+++ MONI/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Windows;
 using MONI.Util;
-using MahApps.Metro;
 
 namespace MONI
 {
@@ -12,7 +10,6 @@ namespace MONI
   public partial class App : Application
   {
     protected override void OnStartup(StartupEventArgs e) {
-      ThemeManager.MainResourceDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/MONI;component/Resources/Controls.xaml") });
       AppHelper.Instance.ConfigureApp(this, Assembly.GetExecutingAssembly().GetName().Name);
       base.OnStartup(e);
     }
