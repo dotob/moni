07a3184ecf00d03f4bd0df4b5eaed0897a8289f1
sebastian krämer <sk@dotob.de>
2014-07-09 16:29:22 +0200

    new version tag 0994

--- MONI/Properties/AssemblyInfo.cs
+++ MONI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("basti & co 4 inform")]
 [assembly: AssemblyProduct("moni")]
-[assembly: AssemblyCopyright("Copyright © basti & co 2013")]
+[assembly: AssemblyCopyright("Copyright © basti & co 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.3")]
-[assembly: AssemblyFileVersion("0.9.9.3")]
+[assembly: AssemblyVersion("0.9.9.4")]
+[assembly: AssemblyFileVersion("0.9.9.4")]
